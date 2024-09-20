3802e06ef995e3f69ebd7e924213c3e666113caf
Ryokeen <Ryokeen@mail.de>
2024-09-20 13:35:02 +0200

    Added more examples are compact versions

--- TemplatePlayground/TemplatePlayground.cpp
+++ TemplatePlayground/TemplatePlayground.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "TestClass.h"
 
+//Long readable version
 template<typename ClassType, typename RetType,typename FuncType, typename ParamType1, typename ParamType2>
 class TestTemplate1
 {
@@ -14,6 +15,8 @@ public:
     }
 };
 
+//shorter version
+//template instance can be reused for other methods of an object as long as the signature matches
 template<typename ClassType, typename RetType, typename ParamType1, typename ParamType2>
 class TestTemplate2
 {
@@ -28,6 +31,28 @@ public:
     }
 };
 
+//compact version as the method pointer typedef is not really necessary
+template<typename ClassType, typename FuncType, typename ParamType1, typename ParamType2>
+class TestTemplate3
+{
+public:
+    void TestCall( ClassType& tClass, FuncType f, ParamType1 x, ParamType2 y ) const
+    {
+        ( ( tClass ).*( f ) )( x, y );
+    }
+};
+
+//same as above just with the reusability of TestTemplate2
+template<typename ClassType, typename ParamType1, typename ParamType2>
+class TestTemplate4
+{
+public:
+    template<typename FuncType>
+    void TestCall( ClassType& tClass, FuncType f, ParamType1 x, ParamType2 y ) const
+    {
+        ( ( tClass ).*( f ) )( x, y );
+    }
+};
 
 template<typename ParamType1, typename ParamType2, typename ClassType, typename RetType,
     RetType( ClassType::* FunctionToCall )( ParamType1, ParamType2 ) const >
@@ -40,6 +65,7 @@ int main()
 {
     std::unique_ptr<ITestClass> tClass = std::make_unique<TestClass>();
 
+    std::cout << "-------TestTemplate1-------" << std::endl;
     TestTemplate1<ITestClass, void, decltype(&ITestClass::PrintInt),int, int> test1;
     test1.TestCall(*tClass, &ITestClass::PrintInt, 1, 3);
 
@@ -52,9 +78,20 @@ int main()
     TestTemplate1<ITestClass, void, decltype(&ITestClass::PrintFloat), float, float> test4;
     test4.TestCall( *tClass, &ITestClass::PrintFloat, 1.0f, 3.0f );
 
+    std::cout << "-------TestTemplate2-------" << std::endl;
     TestTemplate2<ITestClass, int, int, int> test5;
     test5.TestCall<decltype( &ITestClass::AddInt )>( *tClass, &ITestClass::AddInt, 1, 3 );
     test5.TestCall<decltype( &ITestClass::SubInt )>( *tClass, &ITestClass::SubInt, 1, 3 );
 
+    std::cout << "-------TestTemplate3-------" << std::endl;
+    TestTemplate3<ITestClass, decltype( &ITestClass::PrintFloat ), float, float> test6;
+    test6.TestCall( *tClass, &ITestClass::PrintFloat, 1.0f, 3.0f );
+
+    std::cout << "-------TestTemplate4-------" << std::endl;
+    TestTemplate4<ITestClass, int, int> test7;
+    test7.TestCall<decltype( &ITestClass::AddInt )>( *tClass, &ITestClass::AddInt, 1, 3 );
+    test7.TestCall<decltype( &ITestClass::SubInt )>( *tClass, &ITestClass::SubInt, 1, 3 );
+
+    std::cout << "-------TestCall Template-------" << std::endl;
     TestCall<int, int, ITestClass, void, &ITestClass::PrintInt>(*tClass, 1, 3);
 }
